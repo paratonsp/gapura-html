a4bd2475afc007a4d0f0354fc398b8e32ccf59c6
paratonsp <paratonsp@gmail.com>
2022-06-03 15:15:46 +0700

    setup

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "039c5708796f3d98d7d7a3d7e6958eb1",
-"index.html": "8cef8a32a96c6536393ad809f866ed7b",
-"/": "8cef8a32a96c6536393ad809f866ed7b",
+"index.html": "0eab6fd410d8acca892a5160f4661232",
+"/": "0eab6fd410d8acca892a5160f4661232",
 "main.dart.js": "9ad0ed5ff523b033a603bd81557dce13",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
