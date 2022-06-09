f42be563d0e2bf19c5729892368418488bdba558
paratonsp <paratonsp@gmail.com>
2022-06-09 09:00:15 +0700

    setup

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -70,9 +70,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c15acd14c5a7620ffad4e3efdcf3bd5d",
-"/": "c15acd14c5a7620ffad4e3efdcf3bd5d",
-"main.dart.js": "1e6ba3bc0d6ab292bbdb0329505b5027",
+"index.html": "29628959ffa5058a543881491d20d756",
+"/": "29628959ffa5058a543881491d20d756",
+"main.dart.js": "c85622dcf007229ae947633e11b4dcb1",
 "manifest.json": "8bc03d8712a4dbcee651cb1744aa1262",
 "version.json": "039c5708796f3d98d7d7a3d7e6958eb1"
 };
